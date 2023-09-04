170ed581064a7e4c04d2c22659351d042f9077a3
Andreas Valent <andreysfn@yandex.ru>
2023-09-05 00:26:12 +0400

    Добавлен store redux

---
+++ src/shared/redux/slices/mainSlice.ts
@@ -0,0 +1,85 @@
+import { createAsyncThunk, createSlice, PayloadAction } from '@reduxjs/toolkit';
+import { getAvailableLanguages, getStaticInterfaceData } from '../../api';
+import { LangListDTO } from '../../api/dto';
+
+export interface LangType {
+  lng: string;
+  lng_name: string;
+}
+
+export const fetchStaticInterfaceData = createAsyncThunk(
+  'main/fetchStaticInterfaceData',
+  async (locale: string) => {
+    return await getStaticInterfaceData(locale);
+  },
+);
+export const initApp = createAsyncThunk('main/initApp', async () => {
+  return {
+    lngList: await getAvailableLanguages(),
+  };
+});
+
+export interface MainState {
+  counter: number | null;
+  title: string | null;
+  text: string | null;
+  isLoading: boolean;
+  langList: Array<LangType>;
+  errCode: number | null;
+  errMessage: string | null;
+}
+
+const mainSlice = createSlice({
+  name: 'main',
+  initialState: {
+    counter: null,
+    isLoading: false,
+    text: '',
+    title: '',
+    langList: [],
+    errCode: null,
+    errMessage: null,
+  } as MainState,
+  reducers: {
+    initCounter: (state, action: PayloadAction<number>) => {
+      state.counter ??= action.payload;
+    },
+    increment: state => {
+      state.counter ??= 0;
+      state.counter++;
+    },
+    decrement: state => {
+      state.counter ??= 0;
+      state.counter--;
+    },
+  },
+  extraReducers: {
+    [initApp.pending]: (state: MainState) => {
+      state.isLoading = true;
+    },
+    [initApp.fulfilled]: (state: MainState, action: PayloadAction<LangListDTO>) => {
+      state.isLoading = false;
+      state.langList = action.payload;
+    },
+    [initApp.rejected]: (state: MainState) => {
+      state.isLoading = false;
+    },
+    [fetchStaticInterfaceData.pending]: (state: MainState) => {
+      state.isLoading = true;
+    },
+    [fetchStaticInterfaceData.fulfilled]: (
+      state: MainState,
+      action: PayloadAction<Record<string, string>>,
+    ) => {
+      state.text = action.payload.text;
+      state.title = action.payload.title;
+      state.isLoading = false;
+    },
+    [fetchStaticInterfaceData.rejected]: (state: MainState) => {
+      state.isLoading = false;
+    },
+  },
+});
+
+export const mainReducer = mainSlice.reducer;
+export const { initCounter, increment, decrement } = mainSlice.actions;
